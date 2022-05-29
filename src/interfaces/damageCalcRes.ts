7a9568fc6db3391dcedec18ab742fbeef34d06a3
ItayElf <thh123321@gmail.com>
2022-05-29 11:07:22 +0300

    allow inifity

--- src/interfaces/damageCalcRes.ts
+++ src/interfaces/damageCalcRes.ts
@@ -5,6 +5,6 @@ export default interface DamageCalcRes {
   max_casualties: number;
   min_casualties: number;
   avg_casualties: number;
-  min_turns_to_kill: number;
-  max_turns_to_kill: number;
+  min_turns_to_kill: number | string;
+  max_turns_to_kill: number | string;
 }
